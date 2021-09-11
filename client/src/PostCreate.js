ab8f1fd8ca54dbe543cf1bf922c480cb21f19251
Stefan Klarmann <stefan@fddb.de>
2021-09-11 11:04:14 +0200

    test

---
+++ client/src/PostCreate.js
@@ -0,0 +1,35 @@
+import React, { useState } from 'react';
+import axios from 'axios';
+
+const PostCreate = () => {
+	const [title, setTitle] = useState('');
+
+	const submitPost = async (event) => {
+		event.preventDefault();
+
+		try {
+			await axios.post('http://posts.com/posts/create', { title });
+		} catch (err) {
+			console.log(err);
+		}
+
+		setTitle('');
+	};
+	return (
+		<div>
+			<form onSubmit={submitPost}>
+				<div className="form-group">
+					<label>Title</label>
+					<input
+						value={title}
+						onChange={(e) => setTitle(e.target.value)}
+						className="form-control"
+					/>
+				</div>
+				<button className="btn btn-primary">Submit</button>
+			</form>
+		</div>
+	);
+};
+
+export default PostCreate;
